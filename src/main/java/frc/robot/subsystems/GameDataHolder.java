b7bb2f1ce767a8fc1b56b9940adee45511d0cf44
timerunner16 <calvin@koose.com>
2025-06-27 20:13:52 -0400

    reef state display

--- src/main/java/frc/robot/subsystems/GameDataHolder.java
+++ src/main/java/frc/robot/subsystems/GameDataHolder.java
@@ -4,7 +4,15 @@
 
 package frc.robot.subsystems;
 
+import java.util.concurrent.atomic.DoubleAccumulator;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.testingdashboard.SubsystemBase;
+import frc.robot.testingdashboard.TDNumber;
+import frc.robot.testingdashboard.TDSendable;
 
 public class GameDataHolder extends SubsystemBase {
   public class ReefState {
@@ -27,10 +35,20 @@ public class GameDataHolder extends SubsystemBase {
   private ReefState[][] m_reefState = new ReefState[4][12];
   private static GameDataHolder m_gameDataHolder;
 
+  private Field2d m_stateField;
+
+  private int m_displayLevel;
+  private TDNumber m_TDdisplayLevel;
+
   /** Creates a new GameDataHolder. */
   private GameDataHolder() {
     super("GameDataHolder");
     ResetReef();
+
+    m_stateField = new Field2d();
+    m_stateField.setRobotPose(-10, 0, Rotation2d.kZero);
+    new TDSendable(this, "Display", "Reef State", m_stateField);
+    m_TDdisplayLevel = new TDNumber(this, "Display", "Current Level", 1);
   }
 
   public static GameDataHolder getInstance() {
@@ -56,6 +74,27 @@ public class GameDataHolder extends SubsystemBase {
     }
   }
 
+  public void SetDisplayLevel(int level) {
+    m_TDdisplayLevel.set(level);
+  }
+
   @Override
-  public void periodic() {}
+  public void periodic() {
+    m_displayLevel = (int)Math.round(m_TDdisplayLevel.get());
+    if (m_displayLevel < 1) m_displayLevel = 1;
+    if (m_displayLevel > 4) m_displayLevel = 4;
+    for (int i = 0; i < 12; i++) {
+      if (!GetState((int)m_displayLevel, i).filled) continue;
+      double a = Math.toRadians(((i/2)/6.0)*360.0-90.0);
+      Translation2d trans = new Translation2d(3,0)
+        .rotateBy(new Rotation2d(a))
+        .plus(new Translation2d(0,i%2==0?-2./3.:2./3.).rotateBy(new Rotation2d(a)));
+      trans = new Translation2d(trans.getX()+6, -trans.getY()+6);
+      Pose2d pose = new Pose2d(
+        trans,
+        new Rotation2d(Math.PI-a)
+      );
+      m_stateField.getObject(String.valueOf(i)).setPose(pose);
+    }
+  }
 }
